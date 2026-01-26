49b2bc5e4748392955067468f3bcde8531282cd3
aashildf <faas0825@gmail.com>
2026-01-26 14:41:17 +0100

    Laget mappestruktur for pages og components, og startet på Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Hero from "../components/Hero";
+
+export default function Home(){
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        // legge til grid med spill-kort
+      </main>
+      <Footer />
+    </>
+  );
+}
