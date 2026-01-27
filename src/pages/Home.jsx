0a580d80ee0c023674dfe90a60eb11dbacc4b4be
aashildf <faas0825@gmail.com>
2026-01-27 14:29:23 +0100

    Åshild endret Homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,54 +1,50 @@
 import Hero from "../components/Hero";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import Gamecard from "../components/GameCard";
 
-export default function Home(){
-    const games = [ 
-        {
-        id: 1,
+export default function Home() {
+  const games = [
+    {
+      id: 1,
       title: "Mia-sitt-spill",
       description: " ",
-      image: "/bilde1.jpg" 
+      image: "/bilde1.jpg",
     },
     {
       id: 2,
       title: "Therese-sitt-spill",
       description: " ",
-      image: "/bilde2.jpg"
-    }
+      image: "/bilde2.jpg",
+    },
   ];
-    
 
   return (
-    <div
-      style={{ backgroundColor: "#0b0b0d", color: "white", minHeight: "100vh" }}
-    >
-      <Header />
+    <div style={mainContentStyle}>
+      <Hero />
+      <h2 style={{ textAlign: "center", margin: "40px 0" }}>Våre Spill</h2>
 
-      <main style={{ maxWidth: "1200px", margin: "0 auto", padding: "20px" }}>
-        <Hero />
-        <h2> Overskrift til spillseksjonen</h2>
-        {/* Grid som viser spill-kortene */}
-        <section>
-          {games.map((game) => (
-            <Gamecard
-              key={game.id}
-              title={game.title}
-              description={game.description}
-              image={game.image}
-            />
-          ))}
-        </section>
-      </main>
-      <Footer />
+      {/* Grid som viser spill-kortene */}
+      <section>
+        {games.map((game) => (
+          <Gamecard
+            key={game.id}
+            title={game.title}
+            description={game.description}
+            image={game.image}
+          />
+        ))}
+      </section>
     </div>
   );
 }
 
-// Enkel grid-styling så kortene ikke ligger oppå hverandre
-// const gridStyle = {
-//   display: "grid",
-//   gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
-//   gap: "20px"
-// };
+const mainContentStyle = {
+  maxWidth: "1200px",
+  margin: "0 auto",
+  padding: "20px",
+};
+
+const gridStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+  gap: "20px",
+};
