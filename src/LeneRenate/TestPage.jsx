a0b399c6d7eb6c76aad590bcdc96ccab9bf005da
LeneRenate <Lene.renate.rosseboe@gmail.com>
2026-01-27 14:23:49 +0100

    Route Provider

---
+++ src/LeneRenate/TestPage.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <h1 style={{ color: "green" }}>HOME</h1>
+    </>
+  );
+}
--- src/mia/MemoryGame.jsx
+++ src/mia/MemoryGame.jsx
@@ -1,4 +1,3 @@
-
 function MemoryGame() {
   return (
     <main>
