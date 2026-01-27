c6ab7947985d8e864784adb0a5e218e24d6bcd00
miaravnanger <mia_maroen@hotmail.com>
2026-01-27 14:00:29 +0100

    s

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import MemoryGame from "../mia/MemoryGame";
+import MemoryGame from "../mia/MemoryGame.jsx";
 
 export const router = createBrowserRouter([
   {
