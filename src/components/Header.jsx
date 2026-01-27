ce636920329cb5578ab38ee4f3900b23c0325ce3
aashildf <faas0825@gmail.com>
2026-01-27 14:46:40 +0100

    header og footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header
+      style={{
+        padding: "20px",
+        background: "#1a1a1f",
+        color: "#ff00ff",
+        borderBottom: "1px solid #333",
+      }}
+    >
+      <h1>Her kommer header🕹️</h1>
+    </header>
+  );
+}
