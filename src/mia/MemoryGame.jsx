952feff4ebcf6933b886ec4808f1195450f9a105
miaravnanger <mia_maroen@hotmail.com>
2026-01-29 10:18:57 +0100

    noe logikk

--- src/mia/MemoryGame.jsx
+++ src/mia/MemoryGame.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import "./styles/memory.css";
+import emojiCards from "./data/emojiCards.js";
+import {shuffle} from "./utils/shuffle.js";
 
 function MemoryGame() {
+  const [cards, setCards] = useState(() => {
+    const doubledCards = [...emojiCards, ...emojiCards];
+    return shuffle(doubledCards);
+  });
+
+
   return (
-    <section>
+    <>
       <header className="memory-header">
         <h1>Memory Game</h1>
       </header>
-    </section>
+      <section className="game-board">
+        {cards.map((card, index) => (
+          <div key={index} className="card">
+            {card}
+          </div>
+        ))}
+      </section>
+    </>
   );
 }
 
---
+++ src/mia/data/emojiCards.js
@@ -0,0 +1,5 @@
+
+
+const emojiCards = ["😻","😜","👻","👽","🦄","🤠"]
+
+export default emojiCards
