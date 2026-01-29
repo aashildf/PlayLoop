a6268a9b2c35c14063d1247d7dc9ba41214a5a7e
miaravnanger <mia_maroen@hotmail.com>
2026-01-29 14:56:06 +0100

    tailwind testing

--- src/mia/MemoryGame.jsx
+++ src/mia/MemoryGame.jsx
@@ -1,26 +1,38 @@
 import { useState } from "react";
 import "./styles/memory.css";
-import emojiCards from "./data/emojiCards.js";
+import cardData from "./data/cardData.js";
 import {shuffle} from "./utils/shuffle.js";
 
 function MemoryGame() {
   const [cards, setCards] = useState(() => {
-    const doubledCards = [...emojiCards, ...emojiCards];
+    const doubledCards = [...cardData, ...cardData];
     return shuffle(doubledCards);
   });
 
 
   return (
     <>
-      <header className="memory-header">
-        <h1>Memory Game</h1>
-      </header>
-      <section className="game-board">
-        {cards.map((card, index) => (
-          <div key={index} className="card">
-            {card}
-          </div>
-        ))}
+      <section className="memory-container">
+        <header className="memory-header">
+          <h3>Memory game under produksjon</h3>
+        </header>
+        <section className="game-board">
+          {cards.map((card, index) => (
+            <div
+              key={index}
+              className="w-[180px] h-[200px] perspective-[1000px]"
+            >
+              <div className="w-full h-full relative">
+                <div className="absolute w-full h-full top-0 left-0">
+                  <img src={card.image} alt={card.id} />
+                </div>
+                <div className="absolute w-full h-full top-0 left-0">
+                  <img src="/mia-images/bakside1.jpg" alt="Card back" className="w-full h-full object-cover rounded-lg" />
+                </div>
+              </div>
+            </div>
+          ))}
+        </section>
       </section>
     </>
   );
--- src/mia/data/emojiCards.js
+++
@@ -1,5 +0,0 @@
-
-
-const emojiCards = ["😻","😜","👻","👽","🦄","🤠"]
-
-export default emojiCards
