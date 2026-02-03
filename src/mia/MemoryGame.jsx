d77d0f8f159f6485b2718fe042945133d21c6f10
miaravnanger <mia_maroen@hotmail.com>
2026-02-03 11:30:56 +0100

    rydder/deler opp i komponenter

--- src/mia/MemoryGame.jsx
+++ src/mia/MemoryGame.jsx
@@ -2,6 +2,7 @@ import "./styles/flip.css";
 import { useState, useEffect } from "react";
 import cardData from "./data/cardData.js";
 import { shuffle } from "./utils/shuffle.js";
+import Card from "../Mia/components/Card.jsx";
 
 function MemoryGame() {
   // doubled and shuffled cards
@@ -15,6 +16,10 @@ function MemoryGame() {
   const [matchedIndices, setMatchedIndices] = useState([]);
   const [isLocked, setIsLocked] = useState(false);
 
+  // score 
+  const [score, setScore] = useState(0);
+  const [moves, setMoves] = useState(0);
+
   // card clicks
   function handleCardClick(index) {
     if (isLocked) return;
@@ -25,10 +30,11 @@ function MemoryGame() {
       setFlippedIndices([index]);
     } else if (flippedIndices.length === 1) {
       setIsLocked(true);
+      setMoves((prev) => prev + 1);
       setFlippedIndices([...flippedIndices, index]);
     }
   }
- 
+//  Checking match
   /* eslint-disable react-hooks/set-state-in-effect */
   useEffect(() => {
     if (flippedIndices.length !== 2) return;
@@ -38,11 +44,15 @@ function MemoryGame() {
     const secondCard = cards[secondIndex];
 
     if (firstCard.id === secondCard.id) {
+      // Match
       setMatchedIndices((prev) => [...prev, firstIndex, secondIndex]);
+      setScore((prev)=> prev + 10);
       setFlippedIndices([]);
       setIsLocked(false);
     } else {
+      // No match
       setTimeout(() => {
+        setScore((prev)=> Math.max(prev -2, 0));
         setFlippedIndices([]);
         setIsLocked(false);
       }, 800);
@@ -59,43 +69,23 @@ function MemoryGame() {
           <h3 className="text-xl font-semibold">
             Memory game under produksjon
           </h3>
+          <p>
+            <p>
+              Score: {score} · Moves: {moves}
+            </p>
+          </p>
         </header>
         {/* Game board */}
         <div className="grid grid-cols-4 gap-4">
-          {cards.map((card, index) => (
-            <div
-              key={index}
-              onClick={() => handleCardClick(index)}
-              className="w-45 aspect-9/10 perspective-[1000px] cursor-pointer"
-            >
-              <div
-                className={`w-full h-full relative transition-transform duration-500 preserve-3d ${
-                  flippedIndices.includes(index) ||
-                  matchedIndices.includes(index)
-                    ? ""
-                    : "rotate-y-180"
-                }`}
-              >
-                {/* card front */}
-                <div className="absolute w-full h-full top-0 left-0 backface-hidden">
-                  <img
-                    src={card.image}
-                    alt={card.id}
-                    className="w-full h-full object-cover rounded-lg"
-                  />
-                </div>
-
-                {/* card back */}
-                <div className="absolute w-full h-full top-0 left-0 backface-hidden rotate-y-180">
-                  <img
-                    src="/mia-images/bakside1.jpg"
-                    alt="Card back"
-                    className="w-full h-full object-cover rounded-lg"
-                  />
-                </div>
-              </div>
-            </div>
-          ))}
+       {cards.map((card, index) => (
+        <Card 
+        key={index}
+        card={card}
+        isFlipped={flippedIndices.includes(index)}
+        isMatched={matchedIndices.includes(index)}
+        onClick={()=> handleCardClick(index)}
+        />
+       ))}
         </div>
       </article>
     </div>
