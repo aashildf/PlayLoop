013e6037ddbdddc8392b0e969e58b74976fa32dd
aashildf <faas0825@gmail.com>
2026-02-27 11:51:40 +0100

    Fix: Added basename to router for GitHub Pages

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -16,30 +16,34 @@ import GridSystem from "../pages/design/GridSystem";
 import IconLibrary from "../pages/design/IconLibrary";
 import Highscores from "../pages/Higcscores";
 
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <h1>An error occured</h1>,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "about", element: <About /> },
+        { path: "highscores", element: <Highscores /> },
+        { path: "memorygame", element: <MemoryGame /> },
+        { path: "reactiongame", element: <ReactionGame /> },
+        { path: "testpage", element: <TestPage /> },
 
-export const router = createBrowserRouter([
+        // ⭐ Designsystem-ruter
+        { path: "design", element: <Designsystem /> },
+        { path: "design/animations", element: <Animations /> },
+        { path: "design/buttons", element: <Buttons /> },
+        { path: "design/cards", element: <Cards /> },
+        { path: "design/neon", element: <NeonEffects /> },
+        { path: "design/grid", element: <GridSystem /> },
+        { path: "design/icons", element: <IconLibrary /> },
+
+        { path: "*", element: <h1>404 not found</h1> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    errorElement: <h1>An error occured</h1>,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "about", element: <About /> },
-      { path: "highscores", element: <Highscores /> },
-      { path: "memorygame", element: <MemoryGame /> },
-      { path: "reactiongame", element: <ReactionGame /> },
-      { path: "testpage", element: <TestPage /> },
-
-      // ⭐ Designsystem-ruter
-      { path: "design", element: <Designsystem /> },
-      { path: "design/animations", element: <Animations /> },
-      { path: "design/buttons", element: <Buttons /> },
-      { path: "design/cards", element: <Cards /> },
-      { path: "design/neon", element: <NeonEffects /> },
-      { path: "design/grid", element: <GridSystem /> },
-      { path: "design/icons", element: <IconLibrary /> },
-
-      { path: "*", element: <h1>404 not found</h1> },
-    ],
+    basename: "/PlayLoop",
   },
-]);
+);
